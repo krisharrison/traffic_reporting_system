614d7fca5459de5c082d3da2880f62768de11d8d
krisharrison <kris.n.harrison@gmail.com>
2024-09-17 14:58:00 -0400

    formatting api response

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 api_version = constants.TOMTOM_API_VERSION
 api_key = constants.TOMTOM_API_KEY
 
-response_api = requests.get("https://api.tomtom.com/traffic/services/{api_version}/incidentDetails?key={api_key}&ids={ids}&fields={fields}&t={t}&categoryFilter={categoryFilter}&timeValidityFilter={timeValidityFilter}")
+response_api = requests.get("https://api.tomtom.com/traffic/services/{0}/incidentDetails?key={1}&ids={ids}&fields={fields}&t={t}&categoryFilter={categoryFilter}&timeValidityFilter={timeValidityFilter}").format(0,1)
 
 
 
