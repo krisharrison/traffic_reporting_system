fb11d27f9e09840b282acd54ebc01d637cea60cd
kh <kris.n.harrison@gmail.com>
2024-09-25 21:49:37 -0400

    api fields

--- main.py
+++ main.py
@@ -11,12 +11,14 @@ api_version = constants.TOMTOM_API_VERSION
 api_key = constants.TOMTOM_API_KEY
 
 url = "https://api.tomtom.com/traffic/services/{0}/incidentDetails".format(api_version)
-payload = {"key":api_key,"ids":"","fields":{},"t":"","categoryFilter":"","timeValidityFilter":""}
-api_response = requests.get(url,payload)
+payload = {"key":api_key,"ids":"","fields":"{incidents{type,geometry{type,coordinates}}}","t":"1727310440","categoryFilter":"","timeValidityFilter":""}
+api_response = requests.get(url,params=payload)
 
 
 
 @app.route("/")
 def get_api_response():
-    return api_response.url
+    return api_response.text
+
+
 
