84e4dbcedf30496f94ba92faa600d1dfd9651b10
kh <kris.n.harrison@gmail.com>
2024-09-29 21:54:59 -0400

    bbox parameters

--- main.py
+++ main.py
@@ -11,8 +11,18 @@ api_version = constants.TOMTOM_API_VERSION
 api_key = constants.TOMTOM_API_KEY
 
 url = "https://api.tomtom.com/traffic/services/{0}/incidentDetails".format(api_version)
-payload = {"key":api_key,"ids":"","fields":"{incidents{type,geometry{type,coordinates}}}","categoryFilter":"","timeValidityFilter":""}#,"t":"1727310440"
+payload = {"key":api_key,"ids":"","fields":"{incidents{type,geometry{type,coordinates}}}","categoryFilter":"","timeValidityFilter":""}
 api_response = requests.get(url,params=payload)
+
+#Lat Min
+4.165398
+#Lat Max
+4.165453
+
+#Long min
+80.243528
+#Long Max
+
 
 
 
@@ -22,5 +32,5 @@ def get_api_response_header():
 
 @app.route("/data")
 def get_api_response():
-    return api_response.json()
+    return api_response.text
 
