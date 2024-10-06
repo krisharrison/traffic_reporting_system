0e5ccea25cdf28116fd6c4cf674ae505ea9419b6
krisharrison <kris.n.harrison@gmail.com>
2024-10-06 01:16:03 -0400

    api now successfully returns data

--- main.py
+++ main.py
@@ -12,25 +12,12 @@ api_key = constants.TOMTOM_API_KEY
 
 url = "https://api.tomtom.com/traffic/services/{0}/incidentDetails".format(api_version)
 payload = {"key": api_key,
-           "bbox": "-80.243527,43.141332,-79.975639,43.178831",
+           "bbox": "-80.243527, 43.141332, -79.975639, 43.178831",
            "fields": "{incidents{type,geometry{type,coordinates}}}",
-           "categoryFilter": "", "timeValidityFilter": ""
+           "categoryFilter": "0,1,2,3,4,5,6,7,8,9,10,11,14",
+           "timeValidityFilter": "present"
            }
 api_response = requests.get(url, params=payload)
-
-
-# Brantord
-# Lat Min
-43.141332
-# Lat Max
-43.178831
-
-# Long min
--80.243527
-# Long Max
--79.975639
-
-# Toronto
 
 
 @app.route("/")
@@ -41,4 +28,3 @@ def get_api_response_header():
 @app.route("/data")
 def get_api_response():
     return api_response.text
-
