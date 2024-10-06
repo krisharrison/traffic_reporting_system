1da0e3b1ce9191abc82ad6366fe1864adb318ad8
kh <kris.n.harrison@gmail.com>
2024-10-05 21:51:09 -0400

    bbox

--- main.py
+++ main.py
@@ -11,24 +11,32 @@ api_version = constants.TOMTOM_API_VERSION
 api_key = constants.TOMTOM_API_KEY
 
 url = "https://api.tomtom.com/traffic/services/{0}/incidentDetails".format(api_version)
-payload = {"key":api_key,"ids":"","fields":"{incidents{type,geometry{type,coordinates}}}","categoryFilter":"","timeValidityFilter":""}
-api_response = requests.get(url,params=payload)
-
-#Lat Min
-4.165398
-#Lat Max
-4.165453
-
-#Long min
-80.243528
-#Long Max
+payload = {"key": api_key,
+           "bbox": "-80.243527,43.141332,-79.975639,43.178831",
+           "fields": "{incidents{type,geometry{type,coordinates}}}",
+           "categoryFilter": "", "timeValidityFilter": ""
+           }
+api_response = requests.get(url, params=payload)
 
 
+# Brantord
+# Lat Min
+43.141332
+# Lat Max
+43.178831
+
+# Long min
+-80.243527
+# Long Max
+-79.975639
+
+# Toronto
 
 
 @app.route("/")
 def get_api_response_header():
     return dict(api_response.headers)
+
 
 @app.route("/data")
 def get_api_response():
