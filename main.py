d120606a61ccd1f6458193d704b72729d96e863b
krisharrison <kris.n.harrison@gmail.com>
2024-10-06 17:57:30 -0400

    formatting

--- main.py
+++ main.py
@@ -13,7 +13,15 @@ api_key = constants.TOMTOM_API_KEY
 url = "https://api.tomtom.com/traffic/services/{0}/incidentDetails".format(api_version)
 payload = {"key": api_key,
            "bbox": "-80.243527, 43.141332, -79.975639, 43.178831",
-           "fields": "{incidents{type,geometry{type,coordinates}}}",
+           "fields": """{
+                           incidents{
+                               type,
+                               geometry{
+                                   type,
+                                   coordinates
+                               }
+                            }
+                         }""",
            "categoryFilter": "0,1,2,3,4,5,6,7,8,9,10,11,14",
            "timeValidityFilter": "present"
            }
