3459166b0964d8f889363d51f60130ffbee9c866
kh <kris.n.harrison@gmail.com>
2024-10-06 22:03:18 -0400

    pulling all available fields from tomtom api

--- main.py
+++ main.py
@@ -13,19 +13,17 @@ api_key = constants.TOMTOM_API_KEY
 url = "https://api.tomtom.com/traffic/services/{0}/incidentDetails".format(api_version)
 payload = {"key": api_key,
            "bbox": "-80.243527, 43.141332, -79.975639, 43.178831",
-           "fields": """{
-                           incidents{
-                               type,
-                               geometry{
-                                   type,
-                                   coordinates
-                               }
-                            }
-                         }""",
+           "fields":"{incidents{type,geometry{type,coordinates},properties{id,iconCategory,magnitudeOfDelay,events{description,code,iconCategory},startTime,endTime,from,to,length,delay,roadNumbers,timeValidity,numberOfReports,lastReportTime,tmc{countryCode,tableNumber,tableVersion,direction,points{location,offset}}}}}",
            "categoryFilter": "0,1,2,3,4,5,6,7,8,9,10,11,14",
            "timeValidityFilter": "present"
            }
+
 api_response = requests.get(url, params=payload)
+
+json_decode = api_response.json()
+json_decode
+for item in json_decode.keys():
+    print(json_decode)
 
 
 @app.route("/")
@@ -35,4 +33,4 @@ def get_api_response_header():
 
 @app.route("/data")
 def get_api_response():
-    return api_response.text
+    return api_response.json()
