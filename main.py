d3c2271e7e029480b079fec601142f555e22ac91
kh <kris.n.harrison@gmail.com>
2024-11-04 22:22:56 -0500

    added blueprints and imports to modularize the code base

--- main.py
+++ main.py
@@ -1,30 +1,12 @@
 from flask import Flask
-
+from .app.db import traffic_db
+from .app.api import tomtom_api
+from .app.routes import routes
 
 app = Flask(__name__)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+app.register_blueprint(traffic_db)
+app.register_blueprint(tomtom_api)
+app.register_blueprint(routes)
 
 
