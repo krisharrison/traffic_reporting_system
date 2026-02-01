b80cc04f107a6e9311bad810802f5902b0b64cfd
kh <kris.n.harrison@gmail.com>
2026-01-31 22:58:31 -0500

    adjusted coordinates in api call to for accuracy

--- app/api.py
+++ app/api.py
@@ -16,7 +16,7 @@ try:
     # Url & Payload strings
     url = "https://api.tomtom.com/traffic/services/{0}/incidentDetails".format(api_version)
     payload = {"key": api_key,
-               "bbox": "-80.243527, 43.149550, -79.836727, 43.185858",
+               "bbox": "-80.248545, 43.120274, -79.707925, 43.316571",
                "fields":"{incidents{type,geometry{type,coordinates},properties{id,iconCategory,magnitudeOfDelay,events{description,code,iconCategory},startTime,endTime,from,to,length,delay,roadNumbers,timeValidity,numberOfReports,lastReportTime,tmc{countryCode,tableNumber,tableVersion,direction,points{location,offset}}}}}",
                "categoryFilter": "0,1,2,3,4,5,6,7,8,9,10,11,14",
                "timeValidityFilter": "present"
